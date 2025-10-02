a8a93251305bf5c4bf708a89c823e7494082704e
hodorojasorin <117478156+hodorojasorin@users.noreply.github.com>
2025-10-02 14:09:06 +0300

    update

--- src/test/java/org/example/test/utilis/Driver1.java
+++ src/test/java/org/example/test/utilis/Driver1.java
@@ -1,5 +1,5 @@
 package org.example.test.utilis;
-/*comentarii*/
+/*comentarii1*/
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
